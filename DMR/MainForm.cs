828150d777f07feea47549f17890cbf13dbd3490
Roger Clark <roger@rogerclark.net>
2018-09-02 17:34:56 +1000

    Changes to the Calibration screen, to pre-check if Memory Access Mode is enabled, etc

--- DMR/MainForm.cs
+++ DMR/MainForm.cs
@@ -2910,35 +2910,38 @@ namespace DMR
 		{
 			CommPrgForm commPrgForm;
 			this.closeAllForms();
-			if (DialogResult.OK != MessageBox.Show("Please make sure the GD-77 is in Memory Access mode.\nHold keys SK2 (Blue side key), Green Menu and * when turning on the transceiver", "Enable Memory Access mode", MessageBoxButtons.OKCancel))
+			/*
+			if (DialogResult.OK != MessageBox.Show("Please make sure the GD-77 is in Memory Access mode. Hold keys SK2 (Blue side key), Green Menu and * when turning on the transceiver", "Enable Memory Access mode", MessageBoxButtons.OKCancel))
 			{
 				return;
 			}
-
+			*/
 			MainForm.CommsBuffer = new byte[1024*1024];
 
-			CodeplugComms.CommunicationMode = CodeplugComms.CommunicationType.calibrationRead;
-
+			// Pre-read to see if the calibration area appears to be readable.
+			CodeplugComms.CommunicationMode = CodeplugComms.CommunicationType.dataRead;
 			commPrgForm = new CommPrgForm(true);// true =  close download form as soon as download is complete
 			commPrgForm.StartPosition = FormStartPosition.CenterParent;
-			DialogResult result= commPrgForm.ShowDialog();
+			CodeplugComms.startAddress = 0x8f000;
+			CodeplugComms.transferLength = 0x20;
+			DialogResult result = commPrgForm.ShowDialog();
 			if (MainForm.CommsBuffer[0x8f000] == 0x00 && MainForm.CommsBuffer[0x8f001] == 0x00)
 			{
-				MessageBox.Show("The GD-77 does not seem to be in Memory Access mode\nHold keys SK2 (Blue side key), Green Menu and * when turning on the transceiver.\nand try again");
+				MessageBox.Show(Settings.dicCommon["EnableMemoryAccessMode"]);
 				return;
 			}
+
+
+			CodeplugComms.CommunicationMode = CodeplugComms.CommunicationType.calibrationRead;
+			commPrgForm = new CommPrgForm(true);// true =  close download form as soon as download is complete
+			commPrgForm.StartPosition = FormStartPosition.CenterParent;
+			result= commPrgForm.ShowDialog();
+
 			if (DialogResult.OK == result)
 			{
 				CalibrationForm cf = new CalibrationForm();
 				cf.StartPosition = FormStartPosition.CenterParent;
-				if (DialogResult.OK == cf.ShowDialog())
-				{
-					CodeplugComms.CommunicationMode = CodeplugComms.CommunicationType.calibrationWrite;
-
-					commPrgForm = new CommPrgForm(true);// true =  close download form as soon as download is complete
-					commPrgForm.StartPosition = FormStartPosition.CenterParent;
-					commPrgForm.ShowDialog();
-				}
+				cf.ShowDialog();
 			}
 		}
 
--- Extras/Calibration/CalibrationForm.cs
+++ Extras/Calibration/CalibrationForm.cs
@@ -15,9 +15,6 @@ namespace DMR
 {
 	public partial class CalibrationForm : Form
 	{
-		//private CalibrationData _vhfData;
-		//private CalibrationData _uhfData;
-
 		public CalibrationForm()
 		{
 			InitializeComponent();
@@ -34,8 +31,21 @@ namespace DMR
 
 		private void btnWrite_Click(object sender, EventArgs e)
 		{
-			if (DialogResult.Yes != MessageBox.Show("Writing the calibration data to Radioddity GD-77 or any other compatible radio, could potentially damage your radio.\n\nBy clicking 'Yes' you acknowledge that you use this feature entirely at your own risk", "WARNING", MessageBoxButtons.YesNo, MessageBoxIcon.Hand, MessageBoxDefaultButton.Button2))
+			if (DialogResult.Yes != MessageBox.Show("Writing the calibration data to Radioddity GD-77 or any other compatible radio, could potentially damage your radio. By clicking 'Yes' you acknowledge that you use this feature entirely at your own risk", "WARNING", MessageBoxButtons.YesNo, MessageBoxIcon.Hand, MessageBoxDefaultButton.Button2))
 			{
+				return;
+			}
+
+			// Pre-read to see if the Calibration area appears to be writable
+			CodeplugComms.CommunicationMode = CodeplugComms.CommunicationType.dataRead;
+			CommPrgForm commPrgForm = new CommPrgForm(true);// true =  close download form as soon as download is complete
+			commPrgForm.StartPosition = FormStartPosition.CenterParent;
+			CodeplugComms.startAddress = 0x8f000;
+			CodeplugComms.transferLength = 0x20;
+			DialogResult result = commPrgForm.ShowDialog();
+			if (MainForm.CommsBuffer[0x8f000] == 0x00 && MainForm.CommsBuffer[0x8f001] == 0x00)
+			{
+				MessageBox.Show(Settings.dicCommon["EnableMemoryAccessMode"]);
 				return;
 			}
 
@@ -46,14 +56,25 @@ namespace DMR
 
 			array = DataToByte(this.calibrationBandControlVHF.data);
 			Array.Copy(array, 0, MainForm.CommsBuffer, 0x8F070, calibrationDataSize);
-			this.DialogResult = DialogResult.OK;
-			Close();
+
+			CodeplugComms.CommunicationMode = CodeplugComms.CommunicationType.calibrationWrite;
+
+			commPrgForm = new CommPrgForm(true);// true =  close download form as soon as download is complete
+			commPrgForm.StartPosition = FormStartPosition.CenterParent;
+			commPrgForm.ShowDialog();
 		}
 
 		private void btnClose_Click(object sender, EventArgs e)
 		{
-			this.DialogResult = DialogResult.Cancel;
-			Close();
+			if (Control.ModifierKeys == Keys.Shift)
+			{
+				btnWrite.Visible = true;	
+			}
+			else
+			{
+				this.DialogResult = DialogResult.Cancel;
+				Close();
+			}
 		}
 
 		private CalibrationData ByteToData(byte[] byte_0)
@@ -80,12 +101,6 @@ namespace DMR
 		private void onFormShown(object sender, EventArgs e)
 		{
 			MessageBox.Show("This feature is still in development. It currently only allows the calibration data to be viewed");
-			/*
-			if (DialogResult.Yes != MessageBox.Show("This feature is provided 'as is'. You use it at your own risk.\n\nMaking changes to the flash memory in the Radioddity GD-77 or any other compatible radio, using this feature, could potentially damage your radio.\n\nBy clicking 'Yes' you acknowledge that you use this software entirely at your own risk", "WARNING", MessageBoxButtons.YesNo, MessageBoxIcon.Hand, MessageBoxDefaultButton.Button2))
-			{
-				this.Close();
-			}
-			*/
 		}
 	}
 }
